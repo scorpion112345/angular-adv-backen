0a935d0e3a7c686d48eb9513fa2b756320140410
David <david@raccoondev.studio>
2020-08-24 21:52:09 -0500

    Fin seccion 17

--- controllers/medicos.js
+++ controllers/medicos.js
@@ -1,105 +1,121 @@
-const { response } = require('express')
-const Medico = require('../models/Medico')
+const { response } = require("express");
+const Medico = require("../models/Medico");
 
+const getMedico = async (req, res = response) => {
+  const id = req.params.id;
+  let medico;
+  try {
+    medico = await Medico.findById(id)
+      .populate("usuario", "nombre img")
+      .populate("hospital", "nombre img");
+  } catch (error) {
+    return res.status(400).json({
+      ok: false,
+      msg: "Medico no encontrado",
+    });
+  }
 
+  res.json({
+    ok: true,
+    medico,
+  });
+};
 
 const getMedicos = async (req, res = response) => {
-    const medicos = await Medico.find({})
-        .populate('usuario', 'nombre img')
-        .populate('hospital', 'nombre img');
+  const medicos = await Medico.find({})
+    .populate("usuario", "nombre img")
+    .populate("hospital", "nombre img");
 
-    res.json({
-        ok: true,
-        medicos
-    })
-
-}
+  res.json({
+    ok: true,
+    medicos,
+  });
+};
 
 const crearMedico = async (req, res = response) => {
+  const data = {
+    nombre: req.body.nombre,
+    hospital: req.body.hospital,
+  };
+  const medico = new Medico(data);
+  medico.usuario = req.user.id;
 
-    const data = {
-        nombre: req.body.nombre,
-        hospital: req.body.hospital
-    }
-    const medico = new Medico(data)
-    medico.usuario = req.user.id
-
-
-    try {
-        await medico.save()
-        return res.json({
-            ok: true,
-            medico
-        })
-    } catch (error) {
-        return res.status(500).json({
-            ok: false,
-            msg: "Error inesperado"
-        })
-    }
-}
+  try {
+    await medico.save();
+    return res.json({
+      ok: true,
+      medico,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      ok: false,
+      msg: "Error inesperado",
+    });
+  }
+};
 
 const actualizarMedico = async (req, res = response) => {
-    const id = req.params.id
-    const usuarioId = req.user.id
-    try {
-        const medico = await Medico.findById(id)
-        if (!medico) {
-            return res.status(400).json({
-                ok: false,
-                msg: "no medico encontrado"
-            })
-        }
-
-        const cambiosMedico = {
-            ...req.body,
-            usuario: usuarioId
-        }
-        const medicoActualizado = await Medico.findByIdAndUpdate(id, cambiosMedico, { new: true })
-
-        res.json({
-            ok: true,
-            medico: medicoActualizado
-        })
-    } catch (error) {
-        console.log(error);
-        res.json({
-            ok: false,
-            msg: "Error al crear el medico"
-        })
+  const id = req.params.id;
+  const usuarioId = req.user.id;
+  try {
+    const medico = await Medico.findById(id);
+    if (!medico) {
+      return res.status(400).json({
+        ok: false,
+        msg: "no medico encontrado",
+      });
     }
-}
+
+    const cambiosMedico = {
+      ...req.body,
+      usuario: usuarioId,
+    };
+    const medicoActualizado = await Medico.findByIdAndUpdate(
+      id,
+      cambiosMedico,
+      { new: true }
+    );
+
+    res.json({
+      ok: true,
+      medico: medicoActualizado,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      ok: false,
+      msg: "Error al crear el medico",
+    });
+  }
+};
 
 const borrarMedico = async (req, res = response) => {
-
-    const id = req.params.id
-    try {
-        const medico = await Medico.findById(id)
-        if (!medico) {
-            return res.status(400).json({
-                ok: false,
-                msg: "no medico encontrado"
-            })
-        }
-
-        await Medico.findByIdAndDelete(id)
-
-        res.json({
-            ok: true
-        })
-    } catch (error) {
-        res.json({
-            ok: false,
-            msg: "Error al eliminar medico"
-        })
+  const id = req.params.id;
+  try {
+    const medico = await Medico.findById(id);
+    if (!medico) {
+      return res.status(400).json({
+        ok: false,
+        msg: "no medico encontrado",
+      });
     }
 
+    await Medico.findByIdAndDelete(id);
 
-
-}
+    res.json({
+      ok: true,
+    });
+  } catch (error) {
+    res.json({
+      ok: false,
+      msg: "Error al eliminar medico",
+    });
+  }
+};
 module.exports = {
-    getMedicos,
-    crearMedico,
-    actualizarMedico,
-    borrarMedico
-}
+  getMedico,
+  getMedicos,
+  crearMedico,
+  actualizarMedico,
+  borrarMedico,
+};
--- routes/medicos.js
+++ routes/medicos.js
@@ -1,26 +1,35 @@
-const { Router } = require('express')
-const { getMedicos, crearMedico, actualizarMedico, borrarMedico } = require('../controllers/Medicos')
-const { check } = require('express-validator')
-const { validarCampos } = require('../middlewares/valida-campos')
+const { Router } = require("express");
+const {
+  getMedicos,
+  crearMedico,
+  actualizarMedico,
+  borrarMedico,
+  getMedico,
+} = require("../controllers/Medicos");
+const { check } = require("express-validator");
+const { validarCampos } = require("../middlewares/valida-campos");
 
 const router = Router();
-const { validarJwt } = require('../middlewares/validar-jwt')
+const { validarJwt } = require("../middlewares/validar-jwt");
 
+router.get("/", validarJwt, getMedicos);
 
-router.get('/', getMedicos)
-
-router.post('/', [
+router.post(
+  "/",
+  [
     validarJwt,
-    check('nombre', "El nombre es obligatorio").not().isEmpty(),
-    check('hospital', "El hospital es obligatorio").not().isEmpty().isMongoId(),
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("hospital", "El hospital es obligatorio").not().isEmpty().isMongoId(),
 
-    validarCampos
-], crearMedico)
+    validarCampos,
+  ],
+  crearMedico
+);
 
-router.put('/:id', validarJwt, actualizarMedico)
+router.put("/:id", validarJwt, actualizarMedico);
 
+router.delete("/:id", validarJwt, borrarMedico);
 
-router.delete('/:id', borrarMedico)
+router.get("/:id", validarJwt, getMedico);
 
-
-module.exports = router
+module.exports = router;
